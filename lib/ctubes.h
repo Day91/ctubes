1df30e959ec6fc99ec9ab051d38834295937c9d7
Day91 <applelover436@gmail.com>
2020-10-01 13:16:57 -0400

    Defining process struct

--- lib/ctubes.h
+++ lib/ctubes.h
@@ -1,3 +1,10 @@
 #ifndef CTUBES_H
 #define CTUBES_H
+struct process {
+    int fd;
+    char* (*recvline)();
+    char* (*recvuntil)(char* seq)();
+    ssize_t (*send)(char* data)();
+    ssize_t (*sendline)(char* data)();
+}
 #endif
